e55528eef8b2e0fae630953b99004c367a394603
josh-foster <joshfoster321@hotmail.com>
2021-01-08 10:37:34 +0000

    homepage/navbar set up with resposive styling

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import styles from "./Navbar.module.scss";
+import SearchBox from '../SearchBox'
 
 const Navbar = () => {
   return (
-    <>
-      <p>Navbar works</p>
+    <>  
+      <section className={styles.title}>
+        <h1>Search Movies</h1>
+      </section>
+
+      <section className={styles.filters}>
+        <div className={styles.search}>
+          <SearchBox
+            // activateSearch={activateSearch}
+            // grabMoviesBySearch={grabMoviesBySearch}
+          />
+        </div>
+      </section>
     </>
   );
 };
