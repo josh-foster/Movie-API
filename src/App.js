fdb22c6e2f041c08f494d8e476819bc869c52151
josh-foster <joshfoster321@hotmail.com>
2021-01-08 12:01:17 +0000

    fetch and cleaning data of one movie set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-import logo from './logo.svg';
+import React, { useState ,useEffect } from "react";
 import './App.css';
 import Main from './component/Main'
 import Navbar from './component/Navbar'
 
+
+
 function App() {
+
+  const cleanMovieData = (movie) => {
+    const cleanedMovie = {
+      id: movie.imdbID,
+      title: movie.Title,
+      release: movie.Released,
+      year: movie.Year
+    };
+    return cleanedMovie;
+  }
+
+  // const activateSearch = (searchTerm) => {
+//   if(searchTerm) {
+//     return ``
+//   } else {
+//     return ``
+//   }
+// }
+
+
+
+
+const getMovies = () => {
+  fetch(`http://www.omdbapi.com/?apikey=248d288&t=lord+of+the+rings`)
+  .then((res) => res.json())
+  .then((res) => {
+    const cleanedMovies = cleanMovieData(res);
+    console.log(cleanedMovies);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+}
+
+useEffect(() => {
+  getMovies();
+}, [])
+
   return (
     <>
       <section className="nav">
